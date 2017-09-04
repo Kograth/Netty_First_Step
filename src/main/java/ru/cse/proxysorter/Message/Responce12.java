baba94cbade953490fe015228f3cdfafe1720612
Oleynik <Oleynik@ADM-21.cse.ru>
2017-09-04 17:38:31 +0300

    Добавлен класс ответа1.2. ИС->СУ. Назначение продукта. Ответ

--- src/main/java/ru/cse/proxysorter/Message/Responce12.java
+++ src/main/java/ru/cse/proxysorter/Message/Responce12.java
@@ -10,5 +10,20 @@ package ru.cse.proxysorter.Message;
  * @author Oleynik
  */
 public class Responce12 {
-    
+      
+    private byte Command = 0x00;  
+
+    /**
+     * @return the Command
+     */
+    public byte getCommand() {
+        return Command;
+    }
+
+    /**
+     * @param Command the Command to set
+     */
+    public void setCommand(byte Command) {
+        this.Command = Command;
+    }
 }
