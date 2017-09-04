01f5593c067329dc826a9f4d4d92628da980877d
Oleynik <Oleynik@ADM-21.cse.ru>
2017-09-04 17:32:24 +0300

    Добавлен класс ответа1.2. ИС->СУ. Назначение продукта. Ответ

--- src/main/java/ru/cse/proxysorter/Message/Request11.java
+++ src/main/java/ru/cse/proxysorter/Message/Request11.java
@@ -10,14 +10,142 @@ package ru.cse.proxysorter.Message;
  * @author Oleynik
  */
 public class Request11 {
+    
+    private byte Command = 0x00;
+    private byte[] codePLK = new byte[4];
+    private short weight = 0;
+    private short StateWeight = 0;   
+    private short length = 0;
+    private short width = 0;
+    private short height = 0;
+    private short stateSize  = 0;     
+    private String barcode;     
+    
+    /**
+     * @return the Comand
+     */
+    public byte getCommand() {
+        return Command;
+    }
 
-    protected byte Comand = 0x00;
-    protected byte[] codePLK = new byte[4];
-    protected short weight = 0;
-    protected short StateWeight = 0;   
-    protected short length = 0;
-    protected short width = 0;
-    protected short height = 0;
-    protected short stateSize  = 0;     
-    protected String barcode;     
+    /**
+     * @param Comand the Comand to set
+     */
+    public void setCommand(byte Comand) {
+        this.Command = Comand;
+    }
+
+    /**
+     * @return the codePLK
+     */
+    public byte[] getCodePLK() {
+        return codePLK;
+    }
+
+    /**
+     * @param codePLK the codePLK to set
+     */
+    public void setCodePLK(byte[] codePLK) {
+        this.codePLK = codePLK;
+    }
+
+    /**
+     * @return the weight
+     */
+    public short getWeight() {
+        return weight;
+    }
+
+    /**
+     * @param weight the weight to set
+     */
+    public void setWeight(short weight) {
+        this.weight = weight;
+    }
+
+    /**
+     * @return the StateWeight
+     */
+    public short getStateWeight() {
+        return StateWeight;
+    }
+
+    /**
+     * @param StateWeight the StateWeight to set
+     */
+    public void setStateWeight(short StateWeight) {
+        this.StateWeight = StateWeight;
+    }
+
+    /**
+     * @return the length
+     */
+    public short getLength() {
+        return length;
+    }
+
+    /**
+     * @param length the length to set
+     */
+    public void setLength(short length) {
+        this.length = length;
+    }
+
+    /**
+     * @return the width
+     */
+    public short getWidth() {
+        return width;
+    }
+
+    /**
+     * @param width the width to set
+     */
+    public void setWidth(short width) {
+        this.width = width;
+    }
+
+    /**
+     * @return the height
+     */
+    public short getHeight() {
+        return height;
+    }
+
+    /**
+     * @param height the height to set
+     */
+    public void setHeight(short height) {
+        this.height = height;
+    }
+
+    /**
+     * @return the stateSize
+     */
+    public short getStateSize() {
+        return stateSize;
+    }
+
+    /**
+     * @param stateSize the stateSize to set
+     */
+    public void setStateSize(short stateSize) {
+        this.stateSize = stateSize;
+    }
+
+    /**
+     * @return the barcode
+     */
+    public String getBarcode() {
+        return barcode;
+    }
+
+    /**
+     * @param barcode the barcode to set
+     */
+    public void setBarcode(String barcode) {
+        this.barcode = barcode;
+    }
+
+
 }
