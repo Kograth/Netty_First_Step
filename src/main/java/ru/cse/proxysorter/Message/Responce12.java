df5287a150182e573d510443b143955ff96d7aca
Oleynik <Oleynik@ADM-21.cse.ru>
2017-09-19 18:41:34 +0300

    Вызов Сервиса Алексея

--- src/main/java/ru/cse/proxysorter/Message/Responce12.java
+++ src/main/java/ru/cse/proxysorter/Message/Responce12.java
@@ -13,12 +13,13 @@ import io.netty.buffer.Unpooled;
  * @author Oleynik
  */
 public class Responce12 extends Request4All {
-      
+
+    
     private byte Command    = 0x12;
     public byte msgSTX      = 0x02;
     public byte msgETX      = 0x03;
-    public short codeProduct =1000;
-    public byte ExitNumber   = 0x00;
+    private short codeProduct =1000;
+    private byte ExitNumber   = 0x00;
 
 
     public byte getCommand() {
@@ -32,14 +33,42 @@ public class Responce12 extends Request4All {
         this.Command = Command;
     }
 
+    /**
+     * @return the codeProduct
+     */
+    public short getCodeProduct() {
+        return codeProduct;
+    }
+
+    /**
+     * @param codeProduct the codeProduct to set
+     */
+    public void setCodeProduct(short codeProduct) {
+        this.codeProduct = codeProduct;
+    }
+
+    /**
+     * @return the ExitNumber
+     */
+    public byte getExitNumber() {
+        return ExitNumber;
+    }
+
+    /**
+     * @param ExitNumber the ExitNumber to set
+     */
+    public void setExitNumber(byte ExitNumber) {
+        this.ExitNumber = ExitNumber;
+    }
+      
     @Override
     public ByteBuf ToByte() {
 
         byte[] answer = new byte[]{0x02,0x12,0x43,0x66,0x28,0x03};
         msgSTX  = 0x02;
-        Command = 0x12;
-        codeProduct = 9999;
-        ExitNumber  = 40;
+        setCommand((byte) 0x12);
+        setCodeProduct((short) 9999);
+        setExitNumber((byte) 40);
         msgETX      = 0x03;
 
         ByteBuf buf = Unpooled.buffer();
