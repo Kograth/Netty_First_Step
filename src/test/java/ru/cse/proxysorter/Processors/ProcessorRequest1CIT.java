10cad2f0cb0b67c31dabc3dceb54689c2c02fcb3
Oleynik <Oleynik@ADM-21.cse.ru>
2017-10-19 21:50:54 +0300

    Merge origin/master
    Conflicts:
    	src/main/java/ru/cse/APILk/Service1c/SorterExchange.java
    	src/main/java/ru/cse/APILk/Service1c/SorterExchangePortType.java
    	src/main/java/ru/cse/APILk/Service1c/SorterExchangeSoap12Impl.java
    	src/main/java/ru/cse/APILk/Service1c/SorterExchangeSoapImpl.java
    	src/main/java/ru/cse/proxysorter/ProxySorterBuilder.java

--- src/test/java/ru/cse/proxysorter/Processors/ProcessorRequest1CIT.java
+++ src/test/java/ru/cse/proxysorter/Processors/ProcessorRequest1CIT.java
@@ -4,22 +4,32 @@
  * and open the template in the editor.
  */
 package ru.cse.proxysorter.Processors;
-
-import org.apache.camel.Exchange;
+import org.apache.camel.EndpointInject;
+import org.apache.camel.Produce;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.mock.MockEndpoint;
+import org.apache.camel.test.junit4.CamelTestSupport;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import static org.junit.Assert.*;
 import ru.cse.proxysorter.Message.Request11;
-
 /**
  *
  * @author 1
  */
-public class ProcessorRequest1CIT {
+public class ProcessorRequest1CIT extends CamelTestSupport {
+
     private Request11 Msg11;
+
+    @EndpointInject(uri = "mock:result")
+    protected MockEndpoint resultEndpoint;
+
+    @Produce(uri = "direct:start")
+    protected ProducerTemplate template;
+
     public ProcessorRequest1CIT() {
     }
     
@@ -32,6 +42,7 @@ public class ProcessorRequest1CIT {
     }
     
     @Before
+    @Override
     public void setUp() {
         Msg11 = new Request11();
         Msg11.setBarcode("12314321432143214346");
@@ -46,20 +57,48 @@ public class ProcessorRequest1CIT {
     }
     
     @After
+    @Override
     public void tearDown() {
     }
 
     /**
      * Test of process method, of class ProcessorRequest1C.
+     * @throws java.lang.Exception
      */
     @Test
     public void testProcess() throws Exception {
+        Msg11 = new Request11();
+        Msg11.setBarcode("12314321432143214346");
+        Msg11.setCodePLK(1234568);
+        Msg11.setWeight((short) 0x12d688);
+        Msg11.setLength((short) 1234568);
+        Msg11.setHeight((short) 1234568);
+        Msg11.setWidth((short) 1234568);
+        Msg11.setStateWeight((short) 0x1);
+        Msg11.setStateSize((short) 0x1);
+        
+        
+        resultEndpoint.expectedMessageCount(0);
+        //resultEndpoint.expectedBodiesReceived("44");
+        
         System.out.println("process");
-        Exchange exchange = null;
-        ProcessorRequest1C instance = new ProcessorRequest1C();
-        instance.process(exchange);
+        template.sendBodyAndHeader(Msg11, "foo", "notMatchedHeaderValue");
+        
+        //Exchange exchange = null;
+        //ProcessorRequest1C myProcessor = new ProcessorRequest1C();
+        //instance.process(exchange);
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
+        resultEndpoint.assertIsSatisfied();        
     }
     
+    @Override
+    protected RouteBuilder createRouteBuilder() {
+        return new RouteBuilder() {
+            @Override
+            public void configure() {
+                from("direct:start").process(new ProcessorRequest1C()).to("mock:result");
+            }
+        };
+    }    
 }
