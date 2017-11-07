f92634105d9242c37305686b01386d39f7570043
Kograth <31609373+Kograth@users.noreply.github.com>
2017-11-07 18:04:23 +0300

    Обработка кода 15 с заходом в 1С

---
+++ src/main/java/ru/cse/proxysorter/Processors/Req15to16.java
@@ -0,0 +1,11 @@
+package ru.cse.proxysorter.Processors;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+
+public class Req15to16 implements Processor {
+    @Override
+    public void process(Exchange exchange) throws Exception {
+
+    }
+}
